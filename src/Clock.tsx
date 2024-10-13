2ac3d9cd6270350695fd65a26b7ac895ca943a30
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-10-12 19:53:20 -0500

    Make clock face responsive.

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,6 +1,6 @@
 export function Clock() {
-  const w = 600;
-  const h = 600;
+  const w = 500;
+  const h = 500;
   const cx = w / 2;
   const cy = h / 2;
   const r = Math.min(w, h) / 2 - 20;
@@ -16,9 +16,10 @@ export function Clock() {
 
   return (
     <svg
-      width={w}
-      height={h}
+      width="100%"
+      height="100%"
       viewBox={`0 0 ${w} ${h}`}
+      preserveAspectRatio="xMidYMid meet"
       xmlns="http://www.w3.org/2000/svg"
     >
       {Array.from({ length: 24 }).map((_, i) => {
