b508c2875a4ce637c36894ce58432001d0a57856
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-10-12 21:08:06 -0500

    Add linear clock.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
-import { Clock } from "./Clock";
-
 export function App() {
-  return <Clock />;
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>America/Chicago</th>
+            {Array.from({ length: 24 }).map((_, i) => {
+              const h = i;
+              const [h12, m] =
+                h >= 12 ? ([h - 12, "pm"] as const) : ([h, "am"] as const);
+
+              return (
+                <td>
+                  {h12 == 0 ? 12 : h12}&thinsp;{m}
+                </td>
+              );
+            })}
+          </tr>
+          <tr>
+            <th>America/Chicago</th>
+            {Array.from({ length: 24 }).map((_, i) => (
+              <td>{i}</td>
+            ))}
+          </tr>
+          <tr>
+            <th>UTC</th>
+            {Array.from({ length: 24 }).map((_, i) => (
+              <td>{(i + 5) % 24}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
